26fc8740a5bc6bda9512302277e5b254d9fe2071
khm2938 <khm2938@gmail.com>
2026-02-04 11:30:11 +0900

    [추가] 자바스크립트 반복문

---
+++ section01/chapter01/chapter19.js
@@ -0,0 +1,20 @@
+//클래스속에 멤버변수 반복문을 이용해서 출력
+const person = { 
+name: "김동진", 
+age: 25, 
+tall: 179 
+};  
+
+const personKyes = Object.keys(person);
+console.log(personKeys);
+
+for (let index = 0; index < personKeys.length; index++) {
+  const key = personKeys[index];
+  console.log(` ${key} = ${person[key]}`);
+}
+
+const valueArray = Object.values(person);
+console.log(valueArray);
+for (let index = 0; index < valueArray.length; index++) {
+  console.log(` ${valueArray[index]}`);
+}
