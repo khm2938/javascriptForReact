ba8abbdf78d67641e79d341edce60cc71f93487c
khm2938 <khm2938@gmail.com>
2026-02-04 17:54:41 +0900

    [추가] 구조분해할당 배열분해 변수저장, 객체 분해 변수저장

---
+++ section01/chapter01/chapter25.js
@@ -0,0 +1,33 @@
+//구조분해할당 (배열, 분해, 변수저장)
+let array = [1,2,3];
+let one = array[0];
+let two = array[1];
+let three = array[2];
+
+let [_one, _two, _three = 10, _four = 4] = array; // 디폴트값 설정 가능
+console.log(_one);
+console.log(_two);
+console.log(_three);
+console.log(_four);
+
+//객체 구조분해할당
+let person = { 
+  name: "홍길동", 
+  age: 27, 
+  hobby: "테니스", 
+}; 
+
+let person3 = person;
+console.log(person3 === person);
+
+let {
+  age : myAge,
+  name : myName,
+  hobby : myHobby,
+  extra = "hello"
+} = person;
+let _myAge2 = person.age;
+let _myName = person.name;
+let _myHobby = person.hobby;
+
+console.log(myAge, myName, myHobby, extra);
