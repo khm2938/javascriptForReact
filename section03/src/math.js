41abfb909ff1d67d51b3f6260b3d2124f2ce4183
khm2938 <khm2938@gmail.com>
2026-02-06 09:31:09 +0900

    [생성] 리액트 생성 및 스타트

---
+++ section03/src/math.js
@@ -0,0 +1,12 @@
+//math 모듈 (객체: function 함수선언식, 함수표현식, 화살표함수)
+export default function add(a, b) {
+  return a + b;
+}
+export function sub(a, b) {
+  return a - b;
+}
+export function div(a, b) {
+  return a / b;
+}
+
+
