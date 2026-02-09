0b196cf004675a86fb32d12e17c1fe7327522d39
khm2938 <khm2938@gmail.com>
2026-02-09 11:48:23 +0900

    [생성] 사용자가 정의한 useState()

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,68 +3,13 @@ import { useState } from 'react'
 import Bulb from './components/bulb'
 import Count from './components/Count'
 import Register from './components/Register'; 
- 
-
-function App() { 
-  return ( 
-    <> 
-      <Register /> 
-    </> 
-  ); 
-} 
- 
-export default App;
-
-/*
-export default function App() {
-  return (
-    <>
-     <Count />
-     <Bulb />
-    </>
-  )
-}
-*/
-
-
-
-
-/*
+import HookExam from './components/HookExam';
 
 export default function App(){
-  
-  const [count, setCount] = useState(0);
-
-  let count1 = 0;
-
-  function setCount1(value){
-    count1 = value;
-  }
-
-  count1 = 10; // 틀린 구문
-  setCount1(10) // 맞는 구문
+  return <>
+  <></><HookExam/>
+  </>
 }
 
 
 
-
-
-export default function App() {
-  const obj = [
-    {text: '메일', color:"red"},
-    {text: '메일2', color:"green"},
-    {text: '메일3', color:"blue"},
-  ];
-
-  return (
-    <button>
-      <Button {...obj[0]} />
-      <Button {...obj[1]} />
-      <Button {...obj[2]}> 
-        <div>자식요소</div>
-         </Button>
-    </button>
-  )
-}
-
-*/
