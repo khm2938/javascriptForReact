ea5a7c767d710ea6a275601513465687acd9f093
khm2938 <khm2938@gmail.com>
2026-02-10 14:21:26 +0900

    [단위프로젝트] todoList 단위프로젝트 c,r,u,d 기능 테스트 완료

---
+++ section08/src/component/List.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import '../css/List.css'
+import TodoItem from './TodoItem';
+
+const List = ({ todos, onUpdate, onDelete}) => { 
+  const [search, setSearch] = useState(''); 
+  const onChangeSearch = (e) => { 
+    setSearch(e.target.value); 
+  }; 
+
+    //주의바람
+    const getFilterData = ()=>{
+        if(search === ''){
+            return todos;
+        }
+        return todos.filter((todo)=>{
+            return todo.content.toLowerCase().includes(search.toLowerCase())
+        })
+    }
+
+    const filteredTodos = getFilterData();
+
+  return (
+  <div className="List">
+    <h4>Todo List</h4>
+
+    <input
+      value={search}
+      onChange={onChangeSearch}
+      placeholder="검색어를 입력하세요..."
+    />
+
+    <div className="todos_wrapper">
+      {filteredTodos.map((todo) => (
+        <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete}/>
+      ))}
+    </div>
+  </div>
+);
+
+}; 
+ 
+export default List; 
