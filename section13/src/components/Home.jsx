b715e42763d7843c8e947c6f66e2c5d18498bd58
khm2938 <khm2938@gmail.com>
2026-02-12 17:43:25 +0900

    [생성] 감정일기장 리스트목록 테스트 완성

--- section13/src/components/Home.jsx
+++ section13/src/components/Home.jsx
@@ -1,11 +1,37 @@
-import Footer from "./Footer";
 import Header from "./Header";
+import Button from "./Button";
+import DiaryList from "./DiaryList";
+import { DiaryStateContext } from "../App";
+import { useContext, useState } from "react";
 
-const Home = () => { 
-return <>
-<Header /> 
-<div>Home </div> 
-<Footer />
-</>
-}; 
-export default Home; 
+const Home = () => {
+  //공유props 가져오기
+  const state = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  //이전달, 다음달 이벤트처리
+  const onDecreaseMonth = (e)=>{
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1))
+  }
+  const onIncreaseMonth = (e)=>{
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1))
+  }
+
+  //해당달에 해당되는 일기만 필터링
+  const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime();
+  const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth()+1, 0, 23, 59, 59).getTime();
+  const monthlyData = state.filter((item)=>item.createdDate >= beginTime && item.createdDate <= endTime)
+
+    
+    return <>
+        <Header title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth()+1}월`}
+            leftChild={<Button text={"<이전달"} onClick={onDecreaseMonth} />}
+            rightChild={<Button text={"다음달>"} onClick={onIncreaseMonth} />}
+        />
+
+        <DiaryList monthlyData={monthlyData}/>
+
+    </>
+}
+
+export default Home;
